77bf35e9da46604c52e8720edb80bba1a27dde25
Adam Rogers <arogers0311@gmail.com>
2011-08-26 11:37:45 +1000

    added block decryptor class, both encrypt/decrypt seem to work OK
    LF correction
    
    updated .gitignore

--- CSACryptoServiceProvider.cs
+++ CSACryptoServiceProvider.cs
@@ -20,10 +20,6 @@ namespace CSA
             _enableBlock = enableBlock;
             _enableStream = enableStream;
 
-            
-   
-            
-
         }
         public byte[] Encrypt(byte[] cw, byte[] data, int len)
         {
@@ -61,6 +57,47 @@ namespace CSA
             if (_enableStream)
             {
                 // dvbcsa_stream_xor(key->cws, data, data + 8, len - 8);
+            }
+
+            return data;
+        }
+
+        public byte[] Decrypt(byte[] cw, byte[] data, int len)
+        {
+            // bypassed bitwise operation, not sure if it works
+            // it seems to set alen to length from start of packet to end of last 8 byte block, leaving residue untouched
+            // can probably do it in a more .NET way
+            //uint alen = len & unchecked((uint)~0x7);
+
+            int alen = len;  //TODO: implement length checking, this is only good for 184 byte packets (or multiples of 8 bytes)
+
+            CSAKeyStruct ks = new CSAKeyStruct(cw);
+
+            int i;
+
+            if (len < 8)
+                return data;
+
+            // perform stream decipher
+
+            if (_enableStream)
+            {
+                // dvbcsa_stream_xor(key->cws, data, data + 8, len - 8);
+            }
+
+            // perform block decipher
+
+            if (_enableBlock)
+            {
+                CSABlockDecryptor enc = new CSABlockDecryptor(ks, data, len);
+
+                enc.CSABlockDecrypt(0);
+
+                for (i = 8; i < 183; i += 8) // this needs a look
+                {
+                    enc.CSAXor64(i);
+                    enc.CSABlockDecrypt(i);
+                }
             }
 
             return data;
---
+++ CSAStreamEncryptor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSA
+{
+    class CSAStreamEncryptor
+    {
+        private byte[] _data;
+
+        private const uint _DVBCSA_KEYSBUFF_SIZE = 56;
+        private const uint _DVBCSA_CWBITS_SIZE = 64;
+        private const uint _DVBCSA_DATA_SIZE = 8;
+
+        private CSAKeyStruct _ks;
+        private int _len;
+
+        public CSAStreamEncryptor(CSAKeyStruct ks, byte[] data_in, int len)
+        {
+            _data = data_in;
+
+            _ks = ks;
+
+            _len = len;
+        }
+
+        public void CSAStreamEncrypt()
+        {
+            int i = 0;
+
+            ulong A, B;
+
+            int pqzyx = 0;
+            int cfed = 0;
+
+            A = _ks.cw;
+            B = (_ks.cw + 4);
+
+            for (i = 0; i < 8; i++)
+            {
+                //csa_stream_init_round(iv[i], &A, &B, &pqzyx, &cfed);
+                //csa_stream_init_round(swap_nbl(iv[i]), &A, &B, &pqzyx, &cfed);
+                //csa_stream_init_round(iv[i], &A, &B, &pqzyx, &cfed);
+                //csa_stream_init_round(swap_nbl(iv[i]), &A, &B, &pqzyx, &cfed);
+            }
+
+            for(i = 0; i < _len; i++)	/* 4 round = 1 stream byte */
+            {
+                //static const uint8_t csa_stream_out[16] =
+	            //{
+	                //0x00, 0x55, 0x55, 0x00, 0xaa, 0xff, 0xff, 0xaa,
+	                //0xaa, 0xff, 0xff, 0xaa, 0x00, 0x55, 0x55, 0x00,
+	            //};
+
+                //csa_stream_round(&A, &B, &pqzyx, &cfed);
+                //data[i] ^= csa_stream_out[GETD(cfed) & 0xf] & 0xc0;
+
+                //csa_stream_round(&A, &B, &pqzyx, &cfed);
+                //data[i] ^= csa_stream_out[GETD(cfed) & 0xf] & 0x30;
+
+                //csa_stream_round(&A, &B, &pqzyx, &cfed);
+                //data[i] ^= csa_stream_out[GETD(cfed) & 0xf] & 0x0c;
+
+                //csa_stream_round(&A, &B, &pqzyx, &cfed);
+                //data[i] ^= csa_stream_out[GETD(cfed) & 0xf] & 0x03;
+            }
+        }
+    }
+}
